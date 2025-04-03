21f80c3c0c22fce8bb0725c8d5bf300396a8b3b0
cam-25 <cmarquez28@cps.edu>
2025-04-03 14:38:14 +0000

    h

---
+++ script.js
@@ -0,0 +1,26 @@
+let spot1 = {
+    image: 
+    country: "Mexico",
+    city: "Cancun",
+    cost: "$5,000"
+}
+
+let spot2 = {
+    image: 
+    country: "Italy",
+    city: "Venice",
+    cost: "$7,000"
+}
+
+let spots = [spot1, spot2];
+
+let url = document.querySelector(".url");
+let country = document.querySelector(".country");
+let city = document.querySelector(".city");
+let cost = document.querySelector(".cost");
+let enter = document.querySelector(".enter");
+let display = document.querySelector(".display");
+
+function displaySpots(spot){
+    
+}
